96aadcfec18423353d4048e107a3a5a1c08aedc3
Isa <abazovkjks@gmail.com>
2022-11-25 19:11:13 +0600

    test fix

--- src/redux/store.ts
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import todo from "./todoSlice";
-
-export const store = configureStore({
-  reducer: todo,
-});
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
